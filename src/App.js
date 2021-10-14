2b0d1c7924acf2305988ae3b6b282a92acbbc98d
Peter Armstrong <peterarmstrong999@gmail.com>
2021-10-14 14:59:52 +0100

    trying to get img to show

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 function App(){
-  return <h1>"Welcome to the Pokedex"</h1>
-}
+  return <div>
+          <header>
+            <h1>Welcome to the Pokedex</h1>
+              <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Pidgey image"></img>
+          </header>
+          </div>
+};
+
 export default App;
