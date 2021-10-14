3559d17ad14e70a7135d81ead797a2c13e9172b5
Peter Armstrong <peterarmstrong999@gmail.com>
2021-10-14 17:46:02 +0100

    finished ability list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from "react";
 
 function App(){
-  return <div>
-          <header>
-            <h1>Welcome to the Pokedex</h1>
-              <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Pidgey image"></img>
-          </header>
-          </div>
+  return  <div>
+            <Logo />
+            <BestPokemon />
+            <CaughtPokemon />
+          </div>;
+}
+
+const Logo = () => {
+  const appName = "Pedro's Pokedex";
+  return (
+    <header>
+      <h1>{appName}</h1>
+      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Pidgey image"></img>
+    </header>
+  );
 };
+
+const BestPokemon = () => {
+  const abilities = [`Anticipation`, `Speed`, `Solar-Beam`]
+    return (
+      <div>
+        <p>My favorite pokemon is Deoxys</p>
+          <ul>
+            {abilities.map((ability) => (
+              <li>{ability}</li>
+            ))}
+          </ul>
+      </div>
+  );
+};
+
+const CaughtPokemon = () => {
+  const date = new Date().toLocaleDateString()
+  return (
+    <p>Caught 0 Pokemon on</p>
+  )
+}
 
 export default App;
